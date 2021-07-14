d93a4e7006c7cc5e92ba288dcb279bd28a469055
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-07-14 18:37:04 +0000

    Start to implement commands

--- src/commands.ts
+++ src/commands.ts
@@ -1,32 +1,36 @@
-import {
-  cache as cacheReq,
-} from "./lspExtensions";
+import { wrapCommand } from "./novaUtils";
+import { cache as cacheReq } from "./lspExtensions";
 
 /** For the current document active in the editor tell the Deno LSP to cache
  * the file and all of its dependencies in the local cache. */
-export function cache(
-  _context: nova.ExtensionContext,
-  extensionContext: DenoExtensionContext,
-): Callback {
-  return (uris: DocumentUri[] = []) => {
-    const activeEditor = nova.window.activeTextEditor;
-    const client = extensionContext.client;
-    if (!activeEditor || !client) {
-      return;
-    }
-    return nova.window.withProgress({
-      location: nova.ProgressLocation.Window,
-      title: "caching",
-    }, () => {
-      return client.sendRequest(
-        cacheReq,
-        {
-          referrer: { uri: activeEditor.document.uri.toString() },
-          uris: uris.map((uri) => ({
-            uri,
-          })),
-        },
-      );
-    });
-  };
+export function registerCache(client: LanguageClient) {
+
+  return nova.commands.register(
+    "sciencefidelity.dano.cache",
+    wrapCommand(cache)
+  );
+
+  async function cache(editor: TextEditor) {
+    return (uris: DocumentUri[] = []) => {
+      const activeEditor = nova.window.activeTextEditor;
+      const client = extensionContext.client;
+      if (!activeEditor || !client) {
+        return;
+      }
+      return nova.window.withProgress({
+        location: nova.ProgressLocation.Window,
+        title: "caching",
+      }, () => {
+        return client.sendRequest(
+          cacheReq,
+          {
+            referrer: { uri: activeEditor.document.uri.toString() },
+            uris: uris.map((uri) => ({
+              uri,
+            })),
+          },
+        );
+      });
+    };
+  }
 }

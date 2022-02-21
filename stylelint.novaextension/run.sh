d09ff8e54291df370114eff08796d904ac1e803e
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-21 14:16:56 +0000

    build: set up extension to install dependencies

--- stylelint.novaextension/run.sh
+++ stylelint.novaextension/run.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# note: any output from this script will be used by nova's language server, so it'll break functionality
+
 cd "$WORKSPACE_DIR"
-stylelint --formatter json --stdin-filename
+
+# symlinks have issues when the extension is submitted to the library, so we don't use node_modules/.bin
+
+node \
+  "$INSTALL_DIR/node_modules/.bin/stylelint-lsp" \
+  --stdio \
